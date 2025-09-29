c0fb0e45de3f7876c0cc2af3f939bc06cfec88a0
Камил Алиев <kamilaliev1722@gmail.com>
2025-09-29 21:44:04 +0300

    add

---
+++ NotificationContracts/Contracts/UserRegistered.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NotificationContracts.Contracts
+{
+	public class UserRegistered
+	{
+		public Guid UserId { get; set; }
+		public string Email { get; set; }
+		public string UserName { get; set; }
+		public string Body { get; set; }
+	}
+}
